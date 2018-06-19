7be0557b73d4e850cdfad02e5d6292c31f554d6a
unknown <maryamshokoohhi@gmail.com>
2018-06-18 19:02:26 -0700

    trivia game

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+var number = 30;
+var intervalId;
+var correctanswer;
+var incorrectanswer;
+var unanswered;
+var userpicked;
+//  When the resume button gets clicked, execute the run function.
+$("#startbutton").on("click", run);
+
+function myFunction(optradio1) {
+
+    $("result1").value = optradio1;
+};
+
+
+//  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+    console.log('intervalId', intervalId);
+}
+
+
+
+
+//  The decrement function.
+function decrement() {
+    //  Decrease number by one.
+    number--;
+    //  Show the number in the #show-number tag.
+    $("#timeout").text(number);
+    //  Once number hits zero...
+    if (number === 0) {
+        $("#questionnaire").hide();
+    }
+}
+
+run();
+myFunction()
