3a168bd8130a14c4eb9623fc561d708f2d464339
Mark Carlson <mcarlson@avenuecode.com>
2018-06-18 19:52:04 -0700

    Adds click handler for radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,35 +5,44 @@ var incorrectanswer;
 var unanswered;
 var userpicked;
 //  When the resume button gets clicked, execute the run function.
-$("#startbutton").on("click", run);
+$(document).ready(function() {
 
-function myFunction(optradio1) {
-
-    $("result1").value = optradio1;
-};
-
-
-//  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-    console.log('intervalId', intervalId);
-}
-
-
-
-
-//  The decrement function.
-function decrement() {
-    //  Decrease number by one.
-    number--;
-    //  Show the number in the #show-number tag.
-    $("#timeout").text(number);
-    //  Once number hits zero...
-    if (number === 0) {
-        $("#questionnaire").hide();
+    $("#startbutton").on("click", run);
+    
+    $(".radioBtn").on('click', function(event) {
+       // console.log('event clicked', event);
+       if (event.target.name === 'optradio1') {
+        $("#result1").text(event.target.value);
+       }
+    })
+    // function myFunction(optradio1) {
+    //     console.log('optradio1', optradio1);
+    //     $("result1").value = optradio1;
+    // };
+})
+    
+    
+    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+        console.log('intervalId', intervalId);
     }
-}
-
-run();
-myFunction()
+    
+    
+    
+    
+    //  The decrement function.
+    function decrement() {
+        //  Decrease number by one.
+        number--;
+        //  Show the number in the #show-number tag.
+        $("#timeout").text(number);
+        //  Once number hits zero...
+        if (number === 0) {
+            $("#questionnaire").hide();
+        }
+    }
+    
+    run();
+    //myFunction()
